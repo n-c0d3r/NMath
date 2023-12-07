a4e69c05b15757080ab51561f2f302ba69ebff94
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2023-12-07 19:43:10 +0700

    update submodules

--- cmake/NMath/CheckNCPP.cmake
+++ cmake/NMath/CheckNCPP.cmake
@@ -7,7 +7,7 @@ message(STATUS "<NMath::CheckNCPP> Start checking NCPP")
 #   Main checkings
 #####################################################################################
 # check for ncpp
-if(NOT TARGET NCPP)
+if(NOT TARGET ncpp)
     if(NOT EXISTS "${NMATH_SUBMODULES_DIR}/NCPP")
         if(WIN32)
             execute_process(COMMAND "${NMATH_SCRIPTS_DIR}/check_submodules.bat")
