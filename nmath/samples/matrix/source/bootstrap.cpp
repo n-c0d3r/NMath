602c09bb2c2dbaf0ecbf2b8fb2a53480dc999d59
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-04-04 21:06:37 +0700

    Update bootstrap.cpp

--- nmath/samples/matrix/source/bootstrap.cpp
+++ nmath/samples/matrix/source/bootstrap.cpp
@@ -7,30 +7,17 @@ using namespace nmath;
 
 int main() {
 
-	F_matrix2x2 a2x2 = F_matrix2x2{
+    F_vector3 right = { 1, 2, 3 };
+    F_vector3 up = { 1, 2, 3 };
+    F_vector3 forward = { 1, 2, 3 };
+    F_vector3 translation = { 1, 2, 3 };
 
-		F_vector2{ 1.0f, 2.0f },
-		F_vector2{ 3.0f, 4.0f }
+    F_matrix4x4 transform;
+    transform.right = { right, 0 };
+    transform.up = { up, 0 };
+    transform.forward = { forward, 0 };
+    transform.translation = { translation, 1 };
 
-	};
-    
-	F_matrix4x4 a4x4 = F_matrix4x4{
-
-		F_vector4{ 1.0f, 2.0f, 3.0f, 4.0f },
-		F_vector4{ 2.0f, 3.0f, 4.0f, 5.0f },
-		F_vector4{ 3.0f, 4.0f, 5.0f, 6.0f },
-		F_vector4{ 4.0f, 5.0f, 6.0f, 7.0f }
-	
-	};
-
-    F_matrix4x4 a4x4_dbac = a4x4.dbac();
-	F_matrix2x2 a2x2_dbxy = a4x4.dbac().tl2x2();
-    
-    F_matrix4x4 b4x4 = a4x4 + a4x4_dbac;
-    b4x4 *= -1.0f;
-    
-    F_matrix4x4 c4x4 = b4x4 * a4x4;
-    
 	ncpp::pause_console();
 
 	return 0;
