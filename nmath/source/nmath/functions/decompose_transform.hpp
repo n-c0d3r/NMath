78feaff0aae339dbcb17a451a652abfeef5bf3f6
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-04-10 16:32:23 +0700

    Update decompose_transform.hpp

--- nmath/source/nmath/functions/decompose_transform.hpp
+++ nmath/source/nmath/functions/decompose_transform.hpp
@@ -147,11 +147,7 @@ namespace nmath {
         return {
             .scale = scale,
             .translation = F_vector3_f32::zero(),
-            .rotation_matrix = F_matrix3x3_f32 {
-                dir3x3.a,
-                dir3x3.b,
-                dir3x3.c
-            }
+            .rotation_matrix = dir3x3
         };
     }
     inline TF_decompose_transform<f32> NMATH_CALL_CNV decompose_transform(PA_matrix4x4_f32 transform) noexcept {
@@ -164,23 +160,83 @@ namespace nmath {
             length(transform3x3.c)
         };
 
-        F_vector3_f32 positive_z_vector = cross(transform3x3.a, transform3x3.b);
+        F_matrix3x3_f32 dir3x3;
+        F_matrix3x3_f32 normalized_axes = {
+            transform3x3.a / scale.x,
+            transform3x3.b / scale.y,
+            transform3x3.c / scale.z,
+        };
 
-        if(
-            dot(transform3x3.c, positive_z_vector)
-            <= 0.0f
-        ) {
-            scale.z *= -1.0f;
+        u8 is_zero_scale_x = (scale.x < NMATH_DEFAULT_TOLERANCE_F32);
+        u8 is_zero_scale_y = (scale.y < NMATH_DEFAULT_TOLERANCE_F32);
+        u8 is_zero_scale_z = (scale.z < NMATH_DEFAULT_TOLERANCE_F32);
+        u8 zero_case_index = (
+            is_zero_scale_x
+            | (is_zero_scale_y << 1)
+            | (is_zero_scale_z << 2)
+        );
+
+        switch(zero_case_index) {
+
+            case(0b111):
+                dir3x3 = T_identity<F_matrix3x3_f32>();
+                break;
+
+            case(0b101):
+                dir3x3 = T_normal_to_rotation<F_matrix3x3_f32>(normalized_axes.b);
+                break;
+
+            case(0b110):
+                dir3x3 = T_bitangent_to_rotation<F_matrix3x3_f32>(normalized_axes.a);
+                break;
+
+            case(0b100):
+                dir3x3 = normalized_axes;
+                dir3x3.c = cross(
+                    normalized_axes.a,
+                    normalized_axes.b
+                );
+                break;
+
+            case(0b011):
+                dir3x3 = T_tangent_to_rotation<F_matrix3x3_f32>(normalized_axes.c);
+                break;
+
+            case(0b001):
+                dir3x3 = normalized_axes;
+                dir3x3.a = cross(
+                    normalized_axes.b,
+                    normalized_axes.c
+                );
+                break;
+
+            case(0b010):
+                dir3x3 = normalized_axes;
+                dir3x3.b = cross(
+                    normalized_axes.c,
+                    normalized_axes.a
+                );
+                break;
+
+            case(0b000):
+                dir3x3 = normalized_axes;
+                if(
+                    dot(
+                        normalized_axes.c,
+                        cross(normalized_axes.a, normalized_axes.b)
+                    ) < 0.0f
+                ) {
+                    scale.z *= -1.0f;
+                    dir3x3.c *= -1.0f;
+                }
+                break;
+
         }
 
         return {
             .scale = scale,
             .translation = transform.translation.xyz(),
-            .rotation_matrix = F_matrix3x3_f32 {
-                (scale.x == 0.0f) ? F_vector3_f32::zero() : (transform3x3.a / scale.x),
-                (scale.y == 0.0f) ? F_vector3_f32::zero() : (transform3x3.b / scale.y),
-                (scale.z == 0.0f) ? F_vector3_f32::zero() : (transform3x3.c / scale.z)
-            }
+            .rotation_matrix = dir3x3
         };
     }
 
